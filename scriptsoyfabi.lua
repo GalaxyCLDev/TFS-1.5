b0d7b7520af6e4309561091b4b7fcfd3179a2546
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-08-07 00:30:10 -0400

    Update scriptsoyfabi.lua

--- scriptsoyfabi.lua
+++ scriptsoyfabi.lua
@@ -1,5 +1,38 @@
 local config = {
-    [1] = {
+    ["knight"] = {
+        [25] = 3,
+        [50] = 4,
+        [100] = 5,
+        [150] = 7,
+        [200] = 9,
+        [250] = 10,
+        [300] = 11,
+        [350] = 13,
+        [400] = 14,
+    },
+    ["sorcerer"] = {
+        [25] = 3,
+        [50] = 4,
+        [100] = 5,
+        [150] = 7,
+        [200] = 9,
+        [250] = 10,
+        [300] = 11,
+        [350] = 13,
+        [400] = 14,
+    },
+    ["druid"] = {
+        [25] = 3,
+        [50] = 4,
+        [100] = 5,
+        [150] = 7,
+        [200] = 9,
+        [250] = 10,
+        [300] = 11,
+        [350] = 13,
+        [400] = 14,
+    },
+    ["paladin"] = {
         [25] = 3,
         [50] = 4,
         [100] = 5,
@@ -12,26 +45,33 @@ local config = {
     },
 }
 
+local function unlockSagaRewards(player, outfitId)
+    player:addOutfit(outfitId)
+    player:sendTextMessage(MESSAGE_INFO_DESCR, "You have unlocked a new saga by reaching the level %d.")
+end
+
 local advanceSave = CreatureEvent("SagaOutfit")
 function advanceSave.onAdvance(player, skill, oldLevel, newLevel)
     if skill ~= SKILL_LEVEL or newLevel <= oldLevel then
         return true
     end
 
-    local saga = config[player:getVocation():getId()]
+    local vocation = player:getVocation():getName():lower()
+    local saga = config[vocation]
     if saga then
         local highestUnlockedLevel = player:getStorageValue(1000) or 0
+        local newUnlockedLevel = highestUnlockedLevel
+
         for level, outfitId in pairs(saga) do
             if newLevel >= level and level > highestUnlockedLevel then
-                highestUnlockedLevel = level
+                newUnlockedLevel = level
             end
         end
 
-        if highestUnlockedLevel > player:getStorageValue(1000) then
-            player:setStorageValue(1000, highestUnlockedLevel)
-            local outfitId = saga[highestUnlockedLevel]
-            player:addOutfit(outfitId)
-            player:sendTextMessage(MESSAGE_INFO_DESCR, "You have unlocked a new saga by reaching the level " .. highestUnlockedLevel .. ".")
+        if newUnlockedLevel > highestUnlockedLevel then
+            player:setStorageValue(1000, newUnlockedLevel)
+            local outfitId = saga[newUnlockedLevel]
+            unlockSagaRewards(player, outfitId)
         end
     end
 
